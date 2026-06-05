012ae62b4df841dd9cc8c6cc95f9ac2181bf36b0
raniapriliani-prill <sabrinanuralifah0211@gmail.com>
2026-06-05 18:33:52 +0700

    backend

---
+++ seeders/20260101-demo-skills.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+async up (queryInterface, Sequelize) {
+  return queryInterface.bulkInsert('Skills', [{
+    name: 'JavaScript',
+    level: 90,
+    category: 'Frontend',
+    user_id: 1
+  },
+  
+  {
+    name: 'Node.js',
+    level: 85,
+    category: 'Backend',
+    user_id: 1
+  },
+  
+  {
+    name: 'MySQL',
+    level: 80,
+    category: 'Database',
+    user_id: 1
+  }], {});
+},
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Skills', null, {});
+  }
+};
